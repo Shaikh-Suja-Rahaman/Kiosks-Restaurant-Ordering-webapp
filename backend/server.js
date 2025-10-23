7d2bf14c9ddbcf893b4bdb55598fff1e034de6b2
Shaikh-Suja-Rahaman <sujarahaman.dms.727@gmail.com>
2025-10-23 19:22:00 +0530

    fix 3

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
   console.log("MongoDB connected successfully")
 
-  app.listen(PORT, '0.0.0.0', () => {
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
 }).catch((err) => console.log("MongoDB connection error", err))
